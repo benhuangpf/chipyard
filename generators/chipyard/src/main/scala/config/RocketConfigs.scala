206209a6ac364cd5d897a28de769a78418c079a1
Ben Huang <benhuangpf@gmail.com>
2024-02-08 14:59:53 +0800

    test

--- generators/chipyard/src/main/scala/config/RocketConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketConfigs.scala
@@ -19,13 +19,9 @@ class TinyRocketConfig extends Config(
   new freechips.rocketchip.subsystem.With1TinyCore ++             // single tiny rocket-core
   new chipyard.config.AbstractConfig)
 
-class RV32TinyRocketConfig extends Config(
+class RV32SmallRocketConfig extends Config(
   new freechips.rocketchip.subsystem.WithRV32 ++            // set RocketTiles to be 32-bit
-  new chipyard.iobinders.WithDontTouchIOBinders(false) ++         // TODO FIX: Don't dontTouch the ports
-  new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++ // use incoherent bus topology
-  new freechips.rocketchip.subsystem.WithNBanks(0) ++             // remove L2$
-  new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
-  new freechips.rocketchip.subsystem.With1TinyCore ++             // single tiny rocket-core
+  new freechips.rocketchip.subsystem.WithNSmallCores(1) ++         // single rocket-core
   new chipyard.config.AbstractConfig)
 
 class UARTTSIRocketConfig extends Config(
